43f1e75a2f37bce043f754267fb6f447e3f17d6b
julie.wav <humphreythegray@gmail.com>
2022-07-04 19:10:13 -0400

    Change styles alt to default and add content

--- variables.js
+++ variables.js
@@ -1,34 +1,37 @@
-var sites = [
-    {
-      "name": "The People's Coalition of Tandy",
-      "url": "https://www.pcotandy.org"
-    },
-    {
-      "name": "Tetso Cube Light Ride",
-      "url": "https://tclr.pcotandy.org"
-    },
-    {
-      "name": "Enhydrax",
-      "url": "https://suneinyneeenan.github.io/Enhydrax/"
-    },
-    {
-      "name": "Katarina Mazur",
-      "url": "https://katarinamazur.com/"
-    },
-    {
-      "name": "Wind Chime Calculator",
-      "url": "https://www.windchimecalculator.org"
-    },
-    {
-      "name": "OS Visualizer",
-      "url": "https://os.pcotandy.org"
-    },
-    {
-      "name": "Ko T. C.",
-      "url": "https://kotc.life/"
-    },
-    {
-      "name": "Julie Bodian",
-      "url": "https://www.julipode.net"
-    }
-  ]
+var sites = [{
+    "name": "The Tandy Webring",
+    "url": "https://webring.pcotandy.org"
+  },
+  {
+    "name": "The People's Coalition of Tandy",
+    "url": "https://www.pcotandy.org"
+  },
+  {
+    "name": "Tetso Cube Light Ride",
+    "url": "https://tclr.pcotandy.org"
+  },
+  {
+    "name": "Enhydrax",
+    "url": "https://suneinyneeenan.github.io/Enhydrax/"
+  },
+  {
+    "name": "Katarina Mazur",
+    "url": "https://katarinamazur.com/"
+  },
+  {
+    "name": "Wind Chime Calculator",
+    "url": "https://www.windchimecalculator.org"
+  },
+  {
+    "name": "OS Visualizer",
+    "url": "https://os.pcotandy.org"
+  },
+  {
+    "name": "Ko T. C.",
+    "url": "https://kotc.life/"
+  },
+  {
+    "name": "Julie Bodian",
+    "url": "https://www.julipode.net"
+  }
+]
